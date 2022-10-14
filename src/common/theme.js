50df286b8e0f6573301b2eb3c65614af2730670e
廖晨竹 <vermouth0226@gmail.com>
2022-10-14 11:28:40 +0800

    add theme: azure

--- src/common/theme.js
+++ src/common/theme.js
@@ -19,6 +19,13 @@ const forestTheme = {
   badgeTextColor: "#87E3C9",
 };
 
+const azureTheme = {
+  bgColor: "#78C5DC",
+  titleColor: "#D8F4EF",
+  textColor: "#C5F7EB",
+  badgeTextColor: "#F5B9AB",
+};
+
 const maltTheme = {
   bgColor: "#E8D4C2",
   titleColor: "#CEC411",
@@ -34,6 +41,8 @@ const useTheme = (theme) => {
       return greeneryTheme;
     case "forest":
       return forestTheme;
+    case "azure":
+      return azureTheme;
     default:
       return defaultTheme;
   }
