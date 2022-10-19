7a40ee1298e36f2396f52f5f4383e83f4872bfa6
Natalie Stroud <stroudn1@nku.edu>
2022-10-19 00:47:23 -0400

    Added Spanish translations to translation.js

--- src/translation.js
+++ src/translation.js
@@ -3,6 +3,7 @@ export const statCardLocales = ({ name }) => {
     "statcard.title": {
       cn: `${name} 的 Stack Overflow 数据`,
       en: `${name}'s Stack Overflow Stats`,
+      es: `${name}'s estadísticas de Stack Overflow `,
       kr: `${name} 의 Stack Overflow 통계`,
       nl: `${name}'s Stack Overflow-statistieken`,
       "zh-tw": `${name} 的 Stack Overflow 數據`,
@@ -10,6 +11,7 @@ export const statCardLocales = ({ name }) => {
     "statcard.reputation": {
       cn: `声量`,
       en: `Reputation`,
+      es: `Reputación`,
       kr: `평판`,
       nl: `Reputatie`,
       "zh-tw": `聲量`,
@@ -17,6 +19,7 @@ export const statCardLocales = ({ name }) => {
     "statcard.question": {
       cn: `问题`,
       en: `Questions`,
+      es: `Preguntas`,
       kr: `의문`,
       nl: `Vragen`,
       "zh-tw": `問題`,
@@ -24,6 +27,7 @@ export const statCardLocales = ({ name }) => {
     "statcard.answer": {
       cn: `回答`,
       en: `Answers`,
+      es: `Respuestas`,
       kr: `대답`,
       nl: `Antwoorden`,
       "zh-tw": `回答`,
